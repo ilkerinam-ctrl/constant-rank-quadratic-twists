05023f9075e4bc647772ab6a630a0c8859c60c07
ilkerinam <ilker.inam@gmail.com>
2026-03-28 23:07:36 +0300

    Create frey_table_clean.m

---
+++ data/frey_table_clean.m
@@ -0,0 +1,37 @@
+FreyRec := recformat<
+    label,
+    conductor,
+    twists
+>;
+
+FreyTable := [
+    rec<FreyRec | label := "11a1", conductor := 11,
+        twists := [-43, -39, -35, -19, -7, 13, 17, 21, 29, 33, 41]>,
+
+    rec<FreyRec | label := "14a1", conductor := 14,
+        twists := [-13, 47, -5, 3, 7]>,
+
+    rec<FreyRec | label := "15a1", conductor := 15,
+        twists := [-11, -7, 57]>,
+
+    rec<FreyRec | label := "17a1", conductor := 17,
+        twists := [-15, 57, 61, 65, 5]>,
+
+    rec<FreyRec | label := "19a1", conductor := 19,
+        twists := [67, 71, 151, 3]>,
+
+    rec<FreyRec | label := "20a1", conductor := 20,
+        twists := [73, 77]>,
+
+    rec<FreyRec | label := "34a1", conductor := 34,
+        twists := [-3]>,
+
+    rec<FreyRec | label := "38a1", conductor := 38,
+        twists := [151, 3]>,
+
+    rec<FreyRec | label := "49a1", conductor := 49,
+        twists := []>,
+
+    rec<FreyRec | label := "98a1", conductor := 98,
+        twists := []>
+];
